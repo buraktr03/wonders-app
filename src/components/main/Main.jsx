4fa7a9a6d155a2a28cdabade1dd8894285f4ed67
buraktr03 <turanburak@gmail.com>
2023-07-15 13:55:29 +0300

    final

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,15 @@
+import { data } from "../../helper/data.js";
+import Card from "./Card.jsx";
+import "./Main.scss";
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="card-container">
+      {data.map((item) => (
+        <Card key={item.id} {...item}/>
+      ))}
+    </div>
+  );
+};
 
-export default Main
+export default Main;
